8a3389a6c4e9b872e7b993f1fab6b60786ff29a0
Spencer Evans <github email address>
2017-10-04 16:30:30 -0700

    added error mesages to chore insex and show pages

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,9 +2,11 @@ class TasksController < ApplicationController
   def create
     @chore = Chore.find(params[:chore_id])
     @task = @chore.tasks.build(task_params)
-    @task.save
-
-    redirect_to chore_path(@chore)
+    if @task.save
+      redirect_to chore_path(@chore)
+    else
+      render "chores/show"
+    end
   end
 
   private
