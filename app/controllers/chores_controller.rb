f5bf5e13a39344547adba2d1176ceec63c8f861d
Spencer Evans <github email address>
2017-10-04 10:21:53 -0700

    added new chore form and chores#create method

--- app/controllers/chores_controller.rb
+++ app/controllers/chores_controller.rb
@@ -1,10 +1,20 @@
 class ChoresController < ApplicationController
 
   def index
+    @chore = Chore.new
     @chores = Chore.all
   end
 
+
   def show
     @chore = Chore.find(params[:id])
+  end
+
+  def create
+    @chore = Chore.new
+    @chore.name = params[:chore][:name]
+    @chore.save
+
+    redirect_to chore_path(@chore)
   end
 end
