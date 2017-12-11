6d95d140b1f5c8bc8ff5a51f792f74c2f62c0e0e
Spencer Evans <github email address>
2017-12-11 10:00:58 -0800

    AJAX for msubmission working on #new-task

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -1,8 +1,9 @@
 $(function(){
   $("#new_task").on("submit", function(e){
-    url = this.action
+    const url = this.action;
+    const authToken = $("input[name='authenticity_token']").val();
     data = {
-      'authenticity_token': $("input[name='authenticity_token']").val(),
+      'authenticity_token': authToken,
       'task': {
         'description': $('#task_description').val()
       }
@@ -14,9 +15,21 @@ $(function(){
       url: url,
       data: data,
       success: function(response) {
-        console.log(response);
-      }
-    });
+        const $newEl = $(`
+          <li>
+          <div class="view">
+            <label>${response.description}</label>
+            <form class="button_to" method="post" action="${response.delete_path}>
+              <input type="hidden" name="_method" value="delete">
+              <input class="destroy" type="submit" value="x">
+              <input type="hidden" name="authenticty_token" value="${authToken}">
+            </form>
+          </div>
+          </li>
+          `);
+          $('.todo-list').append($newEl);
+        }
+      });
 
     e.preventDefault()
   })
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,7 +7,8 @@ class TasksController < ApplicationController
     @task = @house_chore.tasks.build(task_params)
 
     if @task.save
-      redirect_to house_chore_path(@house_chore)
+      render :json => JSON::parse(@task.to_json).merge(:delete_path => house_chore_task_path(@house_chore, :id => @task.id)).to_json
+      # redirect_to house_chore_path(@house_chore)
     else
       raise params.inspect
       flash[:notice] = "Task was not saved"
@@ -18,7 +19,7 @@ class TasksController < ApplicationController
 
   def destroy
     @house_chore = HouseChore.find(params[:house_chore_id])
-    @task = Task.find(params[:id])
+    @task = Task.find(params[:task_id] || params[:id])
     @task.destroy
 
     redirect_to house_chore_path(@house_chore)
