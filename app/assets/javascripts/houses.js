e50a088d23f498c75385cae79cc3da936427ed39
Spencer Evans <github email address>
2017-12-13 17:14:36 -0800

    restyling the app/css

--- app/assets/javascripts/houses.js
+++ app/assets/javascripts/houses.js
@@ -1,10 +1,6 @@
 $(function() {
-  document.getElementById("div-view").addEventListener("click", function(e) {
-  	// e.target is the clicked element!
-  	// If it was a list item
-  	if(e.target && e.target.nodeName == "A") {
-  		// List item found!  Output the ID!
-  		console.log("House link ", e.target.id.replace("house-", ""), " was clicked!");
-  	}
+  $('#1234').on('click', function(e) {
+    e.preventDefault()
+    alert("Stop!")
   })
 });
