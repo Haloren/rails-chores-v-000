66470305ecff8b42d4a4fabf121db79383e31c36
Spencer Evans <github email address>
2017-12-16 16:02:05 -0800

    cleaned up user_chores.js

--- app/assets/javascripts/user_chores.js
+++ app/assets/javascripts/user_chores.js
@@ -14,34 +14,20 @@ $(function(){
 
 $(function(){
   $("a.load_user_chores").on("click", function(e){
-    e.preventDefault();
-//     // fire some ajax
-//     // $.ajax({
-//     //   method: "GET",
-//     //   url: this.href,
-//     // }).success(function(response){
-//     //   $("div.user_chores").html(response)
-//       // we'd really want to load that data into the DOM
-//     // });
-//     // get a response
-//     $.get(this.href).success(function(response){
-//       $("div.user_chores").html(response)
-//     })
-//     // load that response into the DOM
+    e.preventDefault();;
+
   $.get(this.href).success(function(json) {
     var $ol = $("div.user_chores ol")
     $ol.html("")
 
-    json.forEach(function(user_chore) {
-      // const houseChore = new HouseChore(user_chore);
-      // $ol.append(houseChore.getHTML())
+      json.forEach(function(user_chore) {
 
-      $ol.append(`<li><h3>${user_chore.user.username} || ${user_chore.house_chore.chore.name} || ${user_chore.due_date} </h3></li>`);
+        $ol.append(`<li><h3>${user_chore.user.username} || ${user_chore.house_chore.chore.name} || ${user_chore.due_date} </h3></li>`);
+      })
     })
   })
-
-  })
 })
+
 
 $(function() {
   $("a.overdue").on( "click", function(e)  {
