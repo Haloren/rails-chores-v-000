c57fb8c4d7c278777518c8c707ee9831c6153058
Spencer Evans <github email address>
2017-12-18 13:17:45 -0800

    added prev/next functionality to Houses resource using Ruby

--- app/models/house.rb
+++ app/models/house.rb
@@ -8,5 +8,34 @@ class House < ApplicationRecord
   has_many :house_chores
   has_many :chores, through: :house_chores
 
+  def next
+  	houses = house_ids_by_name
+  	house_index = houses.find_index(id)
+  	next_house = house_index + 1
+
+  	if next_house < houses.length
+  		houses[next_house]
+  	else
+  		id
+  	end
+  end
+
+  def prev
+  	houses = house_ids_by_name
+  	house_index = houses.find_index(id)
+  	prev_house = house_index -1
+
+  	if prev_house >= 0
+  		houses[prev_house]
+  	else
+  		id
+  	end
+  end
+
+  private
+
+    def house_ids_by_name
+    	@house_ids ||= House.order(:name => :asc).pluck(:id)
+    end
 
 end
