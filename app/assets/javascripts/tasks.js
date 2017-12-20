0b0c1a3e3110e046e057ccaa3fb5b8ee893fa3f5
Spencer Evans <github email address>
2017-12-20 14:08:39 -0800

    tasks AJAX working again

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -2,34 +2,14 @@ function Task(attributes) {
   this.description = attributes.description;
   this.id = attributes.id;
 }
-// class Task {
-//   constructor(id, description) {
-//     this.id = id
-//     this.description = description
-//   }
-// }
-//
-// Task.prototype.taskHtml = function() {
-//   let newHtml = " "
-//   newHtml += `<li class="task" id="task_${this.id}">`
-//   newHtml += `<div class="view">`
-//   newHtml += `<label>${this.description}</label>`
-//   newHtml += `<form class="button_to" method="post" action="/house_chores/<%= @house_chore.id %>/tasks/${this.id}">`
-//   newHtml += `<input type="hidden" name="_method" value="delete">`
-//   newHtml += `<%= hidden_field_tag :authenticity_token, form_authenticity_token %>`
-//   newHtml += `</form>`
-//   newHtml += `</div>`
-//   newHtml += `</li>`
-//   return newHtml
-// };
 
 $(function() {
   Task.templateSource = $('#task-template').html()
   Task.template = Handlebars.compile(Task.templateSource);
-});
+})
 
 
-Task.prototype.renderLI = function() {
+Task.prototype.renderLi = function() {
  return Task.template(this)
 }
 // working AJAX:
@@ -49,10 +29,10 @@ $(function(){
     .success(function(json) {
       $('#task_description').val("");
       var task = new Task(json);
-      var taskLi = task.renderLil()
-      
+      var taskLi = task.renderLi()
 
-      $('ul.todo-list').append(taskLi)
+
+      $('ul.todo-list').append(taskLi);
     })
     .error(function(response) {
       console.log("Error", response)
