0646c13f11cfc5fdcccfd1310be3956e5f0eeeda
Spencer Evans <github email address>
2017-12-20 13:33:34 -0800

    troubleshooting Handlebars task form

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -2,6 +2,26 @@ function Task(attributes) {
   this.description = attributes.description;
   this.id = attributes.id;
 }
+// class Task {
+//   constructor(id, description) {
+//     this.id = id
+//     this.description = description
+//   }
+// }
+//
+// Task.prototype.taskHtml = function() {
+//   let newHtml = " "
+//   newHtml += `<li class="task" id="task_${this.id}">`
+//   newHtml += `<div class="view">`
+//   newHtml += `<label>${this.description}</label>`
+//   newHtml += `<form class="button_to" method="post" action="/house_chores/<%= @house_chore.id %>/tasks/${this.id}">`
+//   newHtml += `<input type="hidden" name="_method" value="delete">`
+//   newHtml += `<%= hidden_field_tag :authenticity_token, form_authenticity_token %>`
+//   newHtml += `</form>`
+//   newHtml += `</div>`
+//   newHtml += `</li>`
+//   return newHtml
+// };
 
 $(function() {
   Task.templateSource = $('#task-template').html()
@@ -29,7 +49,8 @@ $(function(){
     .success(function(json) {
       $('#task_description').val("");
       var task = new Task(json);
-      var taskLi = task.renderLI()
+      var taskLi = task.renderLil()
+      
 
       $('ul.todo-list').append(taskLi)
     })
