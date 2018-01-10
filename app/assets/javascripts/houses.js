ed3ceb61913d450a1516d0bcd708a4b16074456b
Spencer Evans <github email address>
2018-01-10 12:28:17 -0800

    cleaned up codebase of houeses.js

--- app/assets/javascripts/houses.js
+++ app/assets/javascripts/houses.js
@@ -1,31 +1,20 @@
-// $(function() {
-//   $('.house-link_' + house.id).on('click', function(e) {
-//     e.preventDefault();
-//     alert("Yo!")
-//   })
-// });
-
+// exposes house.city(json) on mouseover-event of a.house-link
 (function() {
   $(document).on('mouseover', '.house-link',  function(e) {
-    // e.preventDefault()
     var $href = $(this).context.lastElementChild.attributes.href.value
-
+// AJAX request (using .get)
     $.get($href).success(function(json) {
-
       var $li = $('#house-details_' + this.url.slice(-2))
-      // debugger
       $li.html("")
 
       $li.append(`<h5>${json.city}</h5>`)
     })
-
   })
 })();
 
+// restores a.house-link to natural state
 (function() {
   $(document).on('mouseout', '.house-link',  function(e) {
-    // e.preventDefault()
-
     var $href = $(this).context.lastElementChild.attributes.href.value
 
     $.get($href).success(function(json) {
@@ -38,7 +27,7 @@
   })
 })();
 
-
+// click-event to go to previous house
 (function() {
   $(document).on("click", ".js-prev", function(e) {
 
@@ -75,6 +64,7 @@
   })
 })();
 
+// click event to go to next house
 (function() {
   $(document).on("click", ".js-next", function(e) {
     e.preventDefault();
@@ -101,36 +91,11 @@
         $ul.append(newHtml)
       }
 
-      // $(".choreName").text(data["chore"]["name"]);
       // re-set the id to the current on the link
 
-
       $(".js-next").attr("data-attribute", data["id"]);
-    
       $(".js-prev").attr("data-attribute", data.id - 1);
 
     });
   });
 })();
-
-// function nextBook(){
-//   $(".next_link").on("click", function(e){
-//     e.preventDefault();
-//     var nextId = parseInt($(".next_link").attr("data-attribute")) + 1;
-//     $.get("/books/" + nextId + ".json", function(data) {
-//       $(".bookTitle").text(data["title"]);
-//       $(".bookAuthor").text(data["author"]);
-//       $(".bookSummary").text(data["summary"]);
-//       // re-set the id to current on the link
-//       $(".next_link").attr("data-attribute", data["id"]);
-//       debugger;
-//     });
-//   });
-// }
-//IIFE
-// $(function() {
-//   const target = document
-//   document.getElementById('target').addEventListener('click', function () {
-//
-//   })
-// })
